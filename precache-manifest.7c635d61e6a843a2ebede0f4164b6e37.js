e5618f544c1ef473438ec5358f1b50f72a47ef06
Elmira <corpse.17@mail.ru>
2020-09-10 23:45:17 +0500

    Updates

---
+++ precache-manifest.7c635d61e6a843a2ebede0f4164b6e37.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf6678e778cc9d2c256256343647df0",
+    "url": "/Intranet-Demo-Version/index.html"
+  },
+  {
+    "revision": "f76ab1c8b856bd97b628",
+    "url": "/Intranet-Demo-Version/static/css/2.dc088e6c.chunk.css"
+  },
+  {
+    "revision": "552bdcb52b0b8e532c5a",
+    "url": "/Intranet-Demo-Version/static/css/main.ad76e5f9.chunk.css"
+  },
+  {
+    "revision": "f76ab1c8b856bd97b628",
+    "url": "/Intranet-Demo-Version/static/js/2.879ac3b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Intranet-Demo-Version/static/js/2.879ac3b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "552bdcb52b0b8e532c5a",
+    "url": "/Intranet-Demo-Version/static/js/main.9dda14f7.chunk.js"
+  },
+  {
+    "revision": "ea94618a3be6ee854ef1",
+    "url": "/Intranet-Demo-Version/static/js/runtime-main.a82fe6d1.js"
+  },
+  {
+    "revision": "8e243691c7ac42df634c0d7352d11553",
+    "url": "/Intranet-Demo-Version/static/media/5k Wallpapers for iMac 8.8e243691.jpg"
+  },
+  {
+    "revision": "f32eaf6be0ed6b90c8f895186f544b03",
+    "url": "/Intranet-Demo-Version/static/media/bg.f32eaf6b.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/Intranet-Demo-Version/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/Intranet-Demo-Version/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/Intranet-Demo-Version/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/Intranet-Demo-Version/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/Intranet-Demo-Version/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/Intranet-Demo-Version/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/Intranet-Demo-Version/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/Intranet-Demo-Version/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/Intranet-Demo-Version/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/Intranet-Demo-Version/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/Intranet-Demo-Version/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/Intranet-Demo-Version/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/Intranet-Demo-Version/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/Intranet-Demo-Version/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/Intranet-Demo-Version/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "40127a5e21b8353d8d01232ef17bd221",
+    "url": "/Intranet-Demo-Version/static/media/new-history-forecast-bulk.40127a5e.png"
+  },
+  {
+    "revision": "6b2db445f718cfa1e511b4bf1a90ab94",
+    "url": "/Intranet-Demo-Version/static/media/unnamed.6b2db445.png"
+  }
+]);
